78f55518fab72bd3be1e2ef1fc1d19b51ff849ef
caunhoak <tanndgcd191158@fpt.edu.vn>
2024-03-06 01:42:02 +0700

    update layout

--- my-project-loveShip/components/OrderScreen.js
+++ my-project-loveShip/components/OrderScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { getAllOrders, createOrder } from '../api/ordersApi';
+import { Picker } from '@react-native-picker/picker'; // Import Picker from @react-native-picker/picker
 
 const OrderScreen = () => {
   const [orders, setOrders] = useState([]);
+  const [selectedItem, setSelectedItem] = useState('');
 
   useEffect(() => {
     fetchOrders();
@@ -21,7 +23,7 @@ const OrderScreen = () => {
   const handleCreateOrder = async () => {
     try {
       const newOrder = await createOrder({
-        // Thông tin của đơn hàng mới
+        item: selectedItem,
       });
       setOrders([...orders, newOrder]);
     } catch (error) {
@@ -31,13 +33,23 @@ const OrderScreen = () => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={selectedItem}
+          style={styles.picker}
+          onValueChange={(itemValue) => setSelectedItem(itemValue)}
+        >
+          <Picker.Item label="Item 1" value="item1" />
+          <Picker.Item label="Item 2" value="item2" />
+          <Picker.Item label="Item 3" value="item3" />
+        </Picker>
+      </View>
       <FlatList
         data={orders}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
           <View style={styles.orderItem}>
             <Text>{item.status}</Text>
-            {/* Hiển thị các thông tin khác của đơn hàng */}
           </View>
         )}
       />
@@ -51,6 +63,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  pickerContainer: {
+    marginTop: 50,
+  },
+  picker: {
+    height: 50,
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
   },
   orderItem: {
     padding: 10,
